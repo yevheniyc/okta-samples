e74e86a6236a2456068cb19834d1748d6de9bfbc
arojnuckarin <atipatr@amgen.com>
2017-02-09 13:55:45 -0800

    Updated to Express 4.x, including the dependencies.

--- okta-node-express/index.js
+++ okta-node-express/index.js
@@ -1,18 +1,22 @@
 var express = require('express'),
     connect = require('connect'),
     auth = require('./auth');
+    logger = require('morgan');
+    compress = require('compression');
+    cookieParser = require('cookie-parser');
+    bodyParser = require('body-parser');
+    multer = require('multer');
+    session = require('express-session');
 
 var app = express();
 
-app.configure(function() {
-  app.use(express.logger());
-  app.use(connect.compress());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.session({ secret: "won't tell because it's secret"  }));
-  app.use(auth.initialize());
-  app.use(auth.session());
-});
+app.use(logger());
+app.use(compress());
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({ secret: "won't tell because it's secret"  }));
+app.use(auth.initialize());
+app.use(auth.session());
 
 app.get('/', auth.protected, function (req, res){
 	  res.end("Hello " + req.session.passport.user);
@@ -32,5 +36,13 @@ app.get('/login', auth.authenticate('saml', { failureRedirect: '/', failureFlash
   }
 );
 
-app.listen(process.env.PORT || 3000);
+var port
+port = 80
+
+var server = app.listen(port, function() {
+    var host = server.address().address;
+    var port = server.address().port
+    console.log('Webserver listening at http://%s:%s', host, port)
+})
+
 console.log("Server started");
